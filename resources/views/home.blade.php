65ee93f3c9a04c6179eaeb5560231fb6cbdcf237
Christofer Jungberg <kiffen_@hotmail.com>
2018-04-26 14:41:34 +0200

    setting up base config and views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.standard')
+@section('title', 'home')
+
+@section('content')
+    <h1>hello</h1>
+
+    <p>This is the foodtruck mega site</p>
+@endsection
---
+++ resources/views/layouts/standard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        {{-- <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}"> --}}
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        {{-- This is the main navbar --}}
+        @include('includes.navbar')
+
+        {{-- Page content --}}
+        <div>
+            @yield('content')
+        </div>
+
+        {{-- footer area  --}}
+        @include('includes.footer')
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/main.js') }}"></script>
+    </body>
+</html>
