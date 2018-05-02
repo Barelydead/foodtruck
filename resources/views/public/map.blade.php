1f5b72b2797fcd3540f0fb1c8344e182bd8950e1
Christofer Jungberg <kiffen_@hotmail.com>
2018-05-02 12:58:28 +0200

    get api URL dynamiclly

--- resources/views/public/map.blade.php
+++ resources/views/public/map.blade.php
@@ -9,13 +9,15 @@
 @endsection
 
 <script>
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:8082/projekt/projects/blog/public/api/coordinates');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        var base_url = window.location.origin;
-        locations = JSON.parse(xhr.responseText);
 
+function initMap() {
+    apiUrl = document.URL.slice(0, -3) + "api/coordinates";
+    console.log(apiUrl);
+    fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(locations) {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: locations[0].lat, lng: locations[0].lng},
             zoom: 6
@@ -23,22 +25,19 @@ xhr.onload = function() {
 
         for (var i = 0; i < locations.length; i++) {
             var marker = new google.maps.Marker({
-              position: {lat: locations[i].lat, lng: locations[i].lng},
-              map: map,
-              title: locations[i].name,
-              url: locations[i].url
+                position: {lat: locations[i].lat, lng: locations[i].lng},
+                map: map,
+                title: locations[i].name,
+                url: locations[i].url
             });
 
             google.maps.event.addListener(marker, 'click', function() {
                 window.location.href = this.url;
             });
         }
-    }
-    else {
-        console.log('Request failed.  Returned status of ' + xhr.status);
-    }
-};
-xhr.send();
+      });
+}
+
 </script>
 <script async defer
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCxJWowKDNK7yhJTHHJCDE_8-s60rmWcfI&callback=initMap">
