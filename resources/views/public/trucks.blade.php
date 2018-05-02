f616bd3c30fb42d74891371393ccdea23c144397
Christofer Jungberg <kiffen_@hotmail.com>
2018-05-02 13:01:05 +0200

    responsive layout trucks

--- resources/views/public/trucks.blade.php
+++ resources/views/public/trucks.blade.php
@@ -4,11 +4,12 @@
 @section('content')
             <div class="row d-flex p-3">
             @foreach ($trucks as $truck)
-                <div class="card col-md mx-2">
+                <div class="card col-md-3 mx-4">
                     <div class="card-header">
                         <h2>{{ $truck->name }}</h2>
                     </div>
                     <div class="card-body">
+                        <p class="card-text text-muted">{{ $truck->city }}</p>
                         <p class="card-text">{{ $truck->description }}</p>
                         <a href="{{URL::to("/trucks/$truck->id")}}" class="btn btn-primary">More details</a>
                     </div>
